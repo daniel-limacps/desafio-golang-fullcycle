2beffef04ddc7a4b7f33934edbf174f9d963181c
danielantoniolima <daniel.limacps@gmail.com>
2024-07-20 11:23:36 -0300

    Atualização de desafio

--- main.go
+++ main.go
@@ -2,22 +2,9 @@
 package main
 
 import (
-	"io"
 	"fmt"
-	"net/http"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
-	n, err := io.WriteString(w, "Full Cycle Rocks!!")
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(n)
-}
-
 func main() {
-	http.HandleFunc("/", index)
-	fmt.Println("Server is listening on port 9000...")
-	http.ListenAndServe(":9000", nil)
+	fmt.Println("Full Cycle Rocks!!!")
 }
